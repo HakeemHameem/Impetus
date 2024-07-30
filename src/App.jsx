4d619def67462a84f257d1575aa35a88de46859a
Hameem <123749814+HakeemHameem@users.noreply.github.com>
2024-07-30 18:05:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import Navbar from './components/navbar';
 import WelcomePage from './components/welcomepage-main';
 import './App.css'; // Ensure you import the CSS
 import Footer from './components/footer/footer'
-import MobileNavbar from './components/MobileNavbar/mobile-Navbar'
-import MobileWelcomePage from './components/MobileNavbar/mobile-Navbar'
-import MobileFooter from './components/MobileNavbar/mobile-Navbar'
-import useWindowSize from './components/MobileNavbar/useWindow-size';
 function App() {
   // const { width } = useWindowSize();
   return (
